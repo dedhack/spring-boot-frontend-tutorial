476efda90c6e0ef6d460e82d7e1541652fa64ba7
izharj <eezhar@gmail.com>
2023-03-31 11:41:25 +0800

    added table in home page, and fetched getUsers form backend. added react-router-dom dependency

--- springboot-frontend/src/pages/Home.jsx
+++ springboot-frontend/src/pages/Home.jsx
@@ -1,36 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  const loadUsers = async () => {
+    const result = await axios.get("http://localhost:8080/users");
+    setUsers(result.data);
+  };
+
   return (
     <div className="container ">
       <div className="py-4">
-        <table class="table border shadow">
+        <table className="table border shadow">
           <thead>
             <tr>
               <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Username</th>
+              <th scope="col">Action</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td colspan="2">Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
+            {users.map((user, index) => (
+              <tr>
+                <th scope="row" key={index}>
+                  {index + 1}
+                </th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.username}</td>
+                <td>
+                  <button className="btn btn-primary mx-2">View</button>
+                  <button className="btn btn-outline-primary mx-2">Edit</button>
+                  <button className="btn btn-danger mx-2">Delete</button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
---
+++ springboot-frontend/src/users/AddUser.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddUser = () => {
+  return <div>AddUserForm</div>;
+};
+
+export default AddUser;
